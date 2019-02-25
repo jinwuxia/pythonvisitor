5495a5f69beb3a7b8ae0828edd521f4a56adaeb3
j123kaishichufa <j123kaishichufa@163.com>
2019-02-25 16:33:47 -0500

    remove abc

--- CarElement.py
+++ CarElement.py
@@ -1,13 +1,8 @@
-from abc import ABCMeta, abstractmethod
-
-NOT_IMPLEMENTED = "You should implement this."
 
 class CarElement:
-    __metaclass__ = ABCMeta
 
     def getname(self):
         print("element")
 
-    @abstractmethod
     def accept(self, visitor):
         visitor.visit(self)
--- CarElementVisitor.py
+++ CarElementVisitor.py
@@ -1,10 +1,4 @@
-from abc import ABCMeta, abstractmethod
-
-NOT_IMPLEMENTED = "You should implement this."
 
 class CarElementVisitor:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
     def visit(self, element):
         print(element.getname())
